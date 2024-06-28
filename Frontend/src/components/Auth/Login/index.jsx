3160d4c9c5087f04b76256c97cc9a42d86e23141
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-06-28 17:21:18 -0500

    💄 design ui login page

---
+++ Frontend/src/components/Auth/Login/index.jsx
@@ -0,0 +1,65 @@
+
+export const Login = () => {
+  return (
+    <div className="bg-white dark:bg-gray-900">
+      <div className="flex justify-center h-screen">
+        <div className="hidden bg-cover lg:block lg:w-2/3" style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)' }}>
+          <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
+            <div>
+              <h2 className="text-4xl font-bold text-white">EmployeeHub</h2>
+
+              <p className="max-w-xl mt-3 text-gray-300">Crea y administra tus empleos con esta plataforma intuitiva y amigable para el usuario, gestionar tus empleados es una tarea tediosa con esta plataforma no batallaras mas con este tipo de tareas</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+          <div className="flex-1">
+            <div className="text-center">
+              <h2 className="text-4xl font-bold text-center text-gray-700 dark:text-white">EmployeeHub</h2>
+
+              <p className="mt-3 text-gray-500 dark:text-gray-300">Log into EmployeeHub to access your account</p>
+            </div>
+
+            <div className="mt-8">
+              <form className="space-y-6">
+                <div
+                  className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500"
+                >
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none text-white"
+                  />
+                </div>
+
+                <div
+                  className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500"
+                >
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none text-white"
+                  />
+                </div>
+
+                <div className="mt-6">
+                  <button
+                    className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                    Log in
+                  </button>
+                </div>
+
+              </form>
+              <div className="mt-2">
+                <input type="checkbox" name="tick" id="tick" className="mr-2" />
+                <label htmlFor="tick" className="text-sm text-gray-600 dark:text-gray-200">You are Agree with terms & conditions</label>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
+}
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './Css/index.css'
+import { Login } from './components/Auth/Login/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Login />
   </React.StrictMode>,
 )
