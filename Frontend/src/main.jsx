f5cbdd682690d5ebcfa95db2c370e29f6e3e7a60
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-07-03 07:33:51 -0500

    ✨ Add Category Design

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -9,6 +9,7 @@ import { Home } from './components/Home/index.jsx'
 import { Employee } from './components/Employee/index.jsx'
 import { Category } from './components/Category/index.jsx'
 import { Profile } from './components/Profile/index.jsx'
+import { AddCategory } from './components/AddCategory/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -22,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path='/dashboard/category' element={<Category />} />
           <Route path='/dashboard/employee' element={<Employee />} />
           <Route path='/dashboard/profile' element={<Profile />} />
+          <Route path='/dashboard/add_catgory' element={<AddCategory />} />
         </Route>
         <Route path='*' element={<p>404</p>} />
       </Routes>
