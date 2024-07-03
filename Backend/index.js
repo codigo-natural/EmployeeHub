f4e985d6972010d7c2a3363a621734a5ca56a36b
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-07-03 13:34:59 -0500

    ✨ Add Category API

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import adminRouter from "./Routes/AdminRoutes.js";
+import addCategory from "./Routes/AddCategory.js";
 import { config } from "./config/index.js";
 
 const PORT = config.port;
@@ -15,6 +16,7 @@ app.use(
 );
 app.use(express.json());
 app.use("/auth", adminRouter);
+// app.use("/auth", addCategory);
 
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
