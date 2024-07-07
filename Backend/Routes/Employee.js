613c21db34708d88e3f7f7a822c18f30ee5accac
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-07-06 19:02:08 -0500

    feat: ✨ add image upload functionality with Multer

--- Backend/Routes/Employee.js
+++ Backend/Routes/Employee.js
@@ -1,10 +1,27 @@
 import express from "express";
 import connection from "../utils/db.js";
 import bcrypt from "bcrypt";
+import multer from "multer";
+import path from "path";
 
 const router = express.Router();
 
-router.post("/add_employee", (req, res) => {
+// image upload
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+const upload = multer({ storage: storage });
+// end image upload
+
+router.post("/add_employee", upload.single("image"), (req, res) => {
   const sql = `INSERT  INTO employee (name, email, password, address, salary, image, category_id) VALUES (?)`;
   bcrypt.hash(req.body.password, 10, (err, hash) => {
     if (err) {
@@ -16,7 +33,7 @@ router.post("/add_employee", (req, res) => {
         hash,
         req.body.address,
         req.body.salary,
-        req.body.image,
+        req.file ? req.file.filename : null,
         req.body.category_id,
       ];
       connection.query(sql, [values], (err, result) => {
